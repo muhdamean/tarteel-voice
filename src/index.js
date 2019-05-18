4c5386c4af42d347bf7afd2ba21d9bda7d246135
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2019-05-18 23:16:08 +0300

    Recognition code is now separated into its own client.
    All of the recognition code is not independent of everything else and is
    factored out into a class.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import SocketIo from 'socket.io';
 import http from 'http';
 import express from 'express';
+import fetch from 'node-fetch';
 
-import { startStream, endStream, handleReceivedData } from './speech';
+import { RecognitionClient } from './speech';
 import { startFileSave, endFileSave, handleDataFileSave } from './file';
-import { setCurrentAyah } from './transcribe';
+import { findDiff, setCurrentAyah } from './transcribe';
 
 require('dotenv').config()
 
@@ -18,23 +19,91 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 })
 
+let interimTranscript = '';
+
 io.on('connection', (socket) => {
   console.log('Connected: ', socket.id);
+  socket.recognitionClient = null;
+
+  socket.onPartialResults = (data) => {
+    if (data.results[0]) {
+      socket.emit('speechData', data);
+      // TODO: Should reset transcript somewhere...
+      interimTranscript += data.results[0].alternatives[0].transcript;
+      if (socket.globalOptions.type === 'transcribe') {
+        if (Math.round(data.results[0].stability)) {
+          // console.log(data.results[0].alternatives[0].transcript);
+          findDiff(socket, data.results[0].alternatives[0].transcript);
+        }
+      }
+    }
+  }
+
+  socket.onFinalResults = (data) => {
+    if (data.results[0]) {
+      socket.emit('speechData', data);
+      console.log('Final word');
+      socket.recognitionClient.endStream();
+
+      let finalString =  data.results[0].alternatives[0].transcript;
   
+      if (socket.globalOptions.type === 'recognition') {
+        socket.handleSearch(finalString);
+      } else {
+        socket.emit('nextAyah')
+        socket.recognitionClient.startStream();
+      }
+    }
+  }
+
+  socket.onError = (errorMsg) => {
+    socket.emit('streamError', errorMsg);
+    socket.emit('endStream');
+  }
+
+  socket.handleSearch = (query) => {
+    console.log('Query is: ', query);
+    socket.recognitionClient.endStream();
+    socket.emit('loading', true)
+    query = query.trim();
+    fetch('https://api.iqraapp.com/api/v3.0/search', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        arabicText: query,
+        translation: 'en-hilali',
+        apikey: process.env.IQRA_API_KEY,
+      }),
+    })
+      .then(res => res.json())
+      .then(json => {
+        socket.emit('foundResults', json.result);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
   socket.on("binaryAudioData", (chunk) => {
-    handleReceivedData(chunk);
+    socket.recognitionClient.handleReceivedData(chunk)
     handleDataFileSave(chunk);
   });
 
   socket.on('endStream', () => {
     console.log('Ended!');
-    endStream();
+    socket.recognitionClient.endStream();
     endFileSave();
   });
 
   socket.on('startStream', (options) => {
     console.log('Starting...');
-    startStream(socket, options);
+    socket.globalOptions = options;
+    socket.recognitionClient = new RecognitionClient(socket.onPartialResults, socket.onFinalResults, (err) => {console.log(err)})
+    socket.recognitionClient.startStream()
+
     startFileSave(socket);
   });
 
--- src/speech.js
+++ src/speech.js
@@ -1,132 +1,73 @@
 import speech from '@google-cloud/speech';
-import fetch from 'node-fetch';
-import uuidv1 from 'uuid/v1';
 
-import upload from './aws/upload';
-import { saveItem } from './aws/db';
-import { findDiff } from './transcribe';
+export class RecognitionClient {
+  constructor(onPartialResults, onFinalResults, onError) {
+    this.requestParams = {
+      config: {
+        encoding: 'LINEAR16',
+        sampleRateHertz: 48000,
+        languageCode: 'ar-AE',
+        profanityFilter: false,
+        enableWordTimeOffsets: true,
+      },
+      interimResults: true, // If you want interim results, set this to true
+      singleUtterance: true,
+    };
+    this.onPartialResults = onPartialResults;
+    this.onFinalResults = onFinalResults;
+    this.onError = onError;
+    this.recognizeStream = null;
+    this.speechClient = new speech.SpeechClient({
+      keyFilename: './config/tarteel-236900-2486c1058706.json',
+    }); // Creates a client.
+  }
 
-let speechClient,
-  recognizeStream,
-  partialQuery = '',
-  interimTranscript = '',
-  globalOptions;
-
-speechClient = new speech.SpeechClient({
-  keyFilename: './config/tarteel-236900-2486c1058706.json',
-}); // Creates a client.
-
-export const startStream = (socket, options) => {
-  const request = {
-    config: {
-      encoding: 'LINEAR16',
-      sampleRateHertz: 48000,
-      languageCode: 'ar-AE',
-      profanityFilter: false,
-      enableWordTimeOffsets: true,
-    },
-    interimResults: true, // If you want interim results, set this to true
-    singleUtterance: true,
-  };
-  
-  globalOptions = options;
-
-  // Create a recognize stream
-  recognizeStream = speechClient
-  .streamingRecognize(request)
-  .on('error', (error) => {
-    if (error.code === 11) {
-      socket.emit('endStream');
-      console.log('Duration exceeded 65 seconds');
-      return;
-    }
-    console.log(error);
-    socket.emit('streamError', error);
-    socket.emit('endStream');
-  })
-  .on('data', (data) => handleData(data, socket));
-};
-
-const handleData = (data, socket) => {  
-  if (data.results[0]) {
-    process.stdout.write(
-      data.results[0].alternatives[0]
-        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-        : `\n\nReached transcription time limit, press Ctrl+C\n`
-    );
-
-    socket.emit('speechData', data);
-
-    if (data.results[0].isFinal) {
-      console.log('Final word');
-      endStream();
-      
-      let finalString =  data.results[0].alternatives[0].transcript;
-
-      if (globalOptions.type === 'recognition') {
-        handleSearch(finalString, socket);
-      } else {
-        socket.emit('nextAyah')
-        startStream(socket, globalOptions);
-      }
-    } else {
-      interimTranscript += data.results[0].alternatives[0].transcript;
-      if (globalOptions.type === 'transcribe') {
-        if (Math.round(data.results[0].stability)) {
-          // console.log(data.results[0].alternatives[0].transcript);
-          findDiff(socket, data.results[0].alternatives[0].transcript);
+  startStream = () => {
+    this.recognizeStream = this.speechClient
+      .streamingRecognize(this.requestParams)
+      .on('error', (error) => {
+        if (error.code === 11) {
+          this.onError('Duration exceeded 65 seconds')
+          return;
         }
+        this.onError(error)
+      })
+      .on('data', (data) => this.handleData(data));
+  }
+
+  handleData = (data) => {
+    if (data.results[0]) {
+      process.stdout.write(
+        data.results[0].alternatives[0]
+          ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+          : `\n\nReached transcription time limit, press Ctrl+C\n`
+      );
+      if (data.results[0].isFinal) {
+        this.onFinalResults(data);
+      } else {
+        this.onPartialResults(data);
       }
     }
-    partialQuery = interimTranscript;
-
   }
-  
-}
 
-/**
-* Closes the recognize stream and wipes it
-*/
-export const endStream = () => {
-  if (recognizeStream) {
-    recognizeStream.end();
-    recognizeStream = null;
+  /**
+  * Closes the recognize stream and wipes it
+  */
+  endStream = () => {
+    if (this.recognizeStream) {
+      this.recognizeStream.end();
+      this.recognizeStream = null;
+    }
   }
-}
 
-/**
-* Receives streaming data and writes it to the recognizeStream for transcription
-* 
-* @param {Buffer} data A section of audio data (ArrayBuffer)
-*/
-export const handleReceivedData = (data) => {
-  if (recognizeStream) {
-    recognizeStream.write(data);
+  /**
+  * Receives streaming data and writes it to the recognizeStream for transcription
+  *
+  * @param {Buffer} data A section of audio data (ArrayBuffer)
+  */
+  handleReceivedData = (data) => {
+    if (this.recognizeStream) {
+      this.recognizeStream.write(data);
+    }
   }
 }
-
-export const handleSearch = (query, socket) => {
-  console.log('Query is: ', query);
-  endStream();
-  socket.emit('loading', true)
-  query = query.trim();
-  fetch('https://api.iqraapp.com/api/v3.0/search', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      arabicText: query,
-      translation: 'en-hilali',
-      apikey: process.env.IQRA_API_KEY,
-    }),
-  })
-    .then(res => res.json())
-    .then(json => {
-      socket.emit('foundResults', json.result);
-    })
-    .catch(e => {
-      console.log(e);
-    });
-};
