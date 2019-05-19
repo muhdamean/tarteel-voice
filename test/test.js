0153d5d1cd9e311e4c3578bc0b2eef1e73f03411
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2019-05-19 23:34:46 +0300

    Added multi-client tests
    Added recognition and transcription tests for multiple concurrent
    clients.

--- test/test.js
+++ test/test.js
@@ -23,7 +23,7 @@ let streamNextSecond = (audioBuffer, cb, done, currIteration, numIterations) => 
   let data = readOneSecond(audioBuffer, currIteration);
   cb(data);
 
-  console.log(`Iteration ${currIteration} of ${numIterations}: ${data.length}`);
+  // console.log(`Iteration ${currIteration} of ${numIterations}: ${data.length}`);
 
   if (currIteration < numIterations - 1) {
     setTimeout(streamNextSecond, 1000, audioBuffer, cb, done, currIteration+1, numIterations)
@@ -67,7 +67,7 @@ describe('Basic tests', function () {
     ayahFile.pipe(ayahReader);
   })
 
-  it('recognize test', function (done) {  
+  it('recognize test', function (done) {
     // Set up client1 connection
     client1 = io.connect(socketUrl, options);
 
@@ -83,7 +83,7 @@ describe('Basic tests', function () {
       done();
     })
 
-    client1.on('connect', function(){
+    client1.on('connect', function() {
       client1.emit('startStream', {type: 'recognition'});
       streamAudioInRealtime(ayahData, (data) => {
         client1.emit('binaryAudioData', data);
@@ -93,7 +93,7 @@ describe('Basic tests', function () {
     });
   });
 
-  it('transcribe test', function (done) {  
+  it('transcribe test', function (done) {
     // Set up client1 connection
     client1 = io.connect(socketUrl, options);
 
@@ -174,5 +174,146 @@ describe('Multi Ayat tests', function () {
         client1.emit('endStream');
       })
     });
+  });
+});
+
+describe('Multi client test', function () {
+  this.timeout(120000);
+  let client1, client2,
+      ayatData = [null, null, null],
+      ayatText = ['بسم الله الرحمن الرحيم','الحمد لله رب العالمين','الرحمن الرحيم'];
+
+  before('Loading wavs...', function(done) {
+    let ayatList = ['001001.wav', '001002.wav', '001003.wav'];
+    eachOf(ayatList, (ayahFile, ayahIndex, cb) => {
+      let ayahStream = fs.createReadStream(`test/${ayahFile}`);
+      let ayahReader = wav.Reader()
+
+      ayahReader.on('format', function (format) {
+        toArray(ayahReader, function (err, arr) {
+          ayatData[ayahIndex] = Buffer.concat(arr)
+          console.log(`Loaded ${ayahFile}`);
+          ayahStream.close()
+          cb();
+        })
+      });
+
+      ayahStream.pipe(ayahReader);
+    }, done);
+  })
+
+  it('recognize test', function (done) {
+    // Set up client1 connection
+    client1 = io.connect(socketUrl, options);
+    client2 = io.connect(socketUrl, options);
+    let numReplies = 0;
+
+    // Start both clients at slightly different time intervals
+    client1.on('connect', function() {
+      setTimeout(() => {
+        client1.emit('startStream', {type: 'recognition'});
+          streamAudioInRealtime(ayatData[0], (data) => {
+            client1.emit('binaryAudioData', data);
+          }, () => {
+            client1.emit('endStream');
+          })
+      }, 1000)
+    });
+
+    client2.on('connect', function() {
+      setTimeout(() => {
+        client2.emit('startStream', {type: 'recognition'});
+          streamAudioInRealtime(ayatData[1], (data) => {
+            client2.emit('binaryAudioData', data);
+          }, () => {
+            client2.emit('endStream');
+          })
+      }, 2000)
+    });
+
+    // Check for correct ayah recognition from both clients
+    client1.on('foundResults', (msg) => {
+      expect(msg['matches'][0]['ayahNum']).to.equal(1);
+      expect(msg['matches'][0]['surahNum']).to.equal(1);
+      client1.disconnect();
+      numReplies++;
+      if (numReplies == 2) {
+        done();
+      }
+    })
+
+    client2.on('foundResults', (msg) => {
+      expect(msg['matches'][0]['surahNum']).to.equal(1);
+      expect(msg['matches'][0]['ayahNum']).to.equal(2);
+      client2.disconnect();
+      numReplies++;
+      if (numReplies == 2) {
+        done();
+      }
+    })
+  });
+
+  it('transcribe test', function (done) {
+    // Set up client1 connection
+    client1 = io.connect(socketUrl, options);
+    client2 = io.connect(socketUrl, options);
+    let numReplies = 0, numRepliesClient1 = 0, numRepliesClient2 = 0;
+
+    // Start both clients at slightly different time intervals
+    client1.on('connect', function() {
+      setTimeout(() => {
+        client1.emit('startStream', {type: 'transcribe'});
+        client1.emit('setCurrentAyah', ayatText[0]);
+        streamMultipleAudioInRealtime(ayatData.slice(0,2), (data) => {
+          client1.emit('binaryAudioData', data);
+        }, () => {
+          client1.emit('endStream');
+        })
+      }, 1000)
+    });
+
+    client2.on('connect', function() {
+      setTimeout(() => {
+        client2.emit('startStream', {type: 'transcribe'});
+        client2.emit('setCurrentAyah', ayatText[1]);
+        streamMultipleAudioInRealtime(ayatData.slice(1,3), (data) => {
+          client2.emit('binaryAudioData', data);
+        }, () => {
+          client2.emit('endStream');
+        })
+      }, 2000)
+    });
+
+    // Check for next ayah
+    client1.on('nextAyah', () => {
+      console.log("Next ayah")
+      numReplies++;
+      numRepliesClient1++;
+      if (numRepliesClient1 < 2) {
+        client1.emit('setCurrentAyah', ayatText[numRepliesClient1]);
+      } else {
+        client1.disconnect();
+      }
+
+      if (numReplies == 4) {
+        done();
+      }
+    })
+
+    client2.on('nextAyah', () => {
+      console.log("Next ayah")
+      numReplies++;
+      numRepliesClient2++;
+      if (numRepliesClient2 < 2) {
+        client2.emit('setCurrentAyah', ayatText[1+numRepliesClient2]);
+      } else {
+        client2.disconnect();
+      }
+
+      if (numReplies == 4) {
+        done();
+      }
+    })
+
   });
 });
