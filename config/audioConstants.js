8db3f40d146e84284f02a63f8c50504e4525e5dc
Anas Abou Allaban <anas@abouallaban.info>
2019-06-18 18:31:10 -0400

    Add config folder

---
+++ config/audioConstants.js
@@ -0,0 +1,8 @@
+export const ENCODING = 'LINEAR16';     // Microphone encoding
+export const SAMPLE_RATE_HZ = 48000;    // Audio sampling rate
+export const GC_SAMPLE_RATE_HZ = 44100; // For google cloud speech
+export const CHANNELS = 1;              // Number of input channels to use
+export const BIT_DEPTH = 16;            // 16: HQ, 24: Master, 32: Recording
+export const LANGUAGE_CODE = 'ar-AE';
+export const KEY_FILE_PATH = '../tarteel-gcpkey.json';
+
